fae153a818790d71c830cdc60c89efc77e21ad07
getromandev <getroman.dev@gmail.com>
2021-01-07 20:45:38 -0500

    final solution

---
+++ codeWars/countingSheep.js
@@ -0,0 +1,57 @@
+// [R]epeat 
+/**
+ * @tutorial https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+ * @description - Consider an array/list of sheep where some sheep may be missing from their place. 
+ *                We need a function that counts the number of sheep present in the array (true means present).
+ * @function countSheeps
+ * @param {array} arrayOfSheep - an array filled with boolean values
+ * @returns {integer} - the total amount of TRUE boolean values
+ */
+
+// [E]xample 
+
+//     Input: [true, false, true, true, false]   return 3
+//         >>>  ✅     ❌      ✅   ✅     ❌    
+
+// [A]pproach - Im going to use a non iterative approach using an array method. 
+//              I can always try to naive approach with a loop if the array method doesnt work out.
+
+// [C]ode 
+
+    //TODO: grab the array and store it in a variable for immutability purposes [STATE]
+    //TODO: filter out the array and find all true boolean values 
+    //TODO: filter method returns a new array with the filtered results, check the length
+    //TODO: return the length of the filtered array. 
+
+function countSheeps(arrayOfSheep) {
+    const sheepState = arrayOfSheep;
+    const filteredArray = sheepState.filter(bool => bool === true);
+    
+    console.log(filteredArray.length);
+    return filteredArray.length;
+}
+    
+// [T]est 
+
+countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ]) // should return 17
+
+// [O]ptimization
+
+// ⏲️ Time Complexity (FOCLP): Function call to outside + Operations + Comparisons + Loops + Pointer References 
+
+//     - ⏲️ O(n) Linear Time Complexity where the size of the input arrayOfSheep will operate on every item
+
+// 🛰 Space Complexity: Input Size + Variables + Auxiliary Space 
+
+//     (A) arrayOfSheep                    Input Size                  (N)4bytes
+//     (B) sheepState                      Variables                   4bytes
+//     (C) filteredArray                   Variables                   4bytes
+//     (D) return                          Auxiliary                   4bytes
+//     (E) countSheeps                     Auxiliary                   4bytes 
+
+//     - 🛰 O(N) linear Space Complexity
---
+++ codeWars/grassHopperSummation.js
@@ -0,0 +1,47 @@
+// [R]epeat 
+/**
+ * @tutorial - https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+ * @description - Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+ * @function summation
+ * @param {integer} num - a positive integer
+ * @returns {integer} - the total sumation of from 1 to @param {integer}
+ */
+
+// [E]xample 
+
+//     if I get the number 5 I need to sum >>>> 1 + 2 + 3 + 4 + 5 = 15 
+
+//     so potentially I would need to run a loop starting at the number on up to N 
+//     and on each iteration add ip the numbers 
+
+// [A]pproach - This will be an iterative approach 
+
+// [C]ode
+
+    //TODO: create a variable Total to hold the total # 
+    //TODO: loop through the Nth starting at #1 
+    //TODO: on every interation append the number to the variable
+    //TODO: return the total
+
+// [T]est - summation(8) should return 36
+ 
+// 1st solution
+var summation = function (num) {
+    let total = 0;
+
+    for (let i = 1; i <= num; i++){
+        total += i
+        console.log('i >>>', i)
+    }
+
+    console.log(total)
+    return total
+}
+
+[O]ptimization <<<<
+
+// 2nd solution
+figure out how to use REDUCE Array with the num being an Int. 
+
+summation(5)
+
