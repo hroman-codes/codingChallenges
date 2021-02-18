7778b8011eab2c66b89c16fd5a1fa897e165e677
getromandev <getroman.dev@gmail.com>
2021-02-17 23:02:10 -0500

    final solution

---
+++ codeWars/youCantCodeUnderPressure.js
@@ -0,0 +1,15 @@
+/**
+ * @tutorial https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+ * @description - code as fast as you can to double the integer and return it
+ * @function doubleInteger
+ * @param {Integer} i
+ * @returns {Integer} - returns the i * 2
+ */
+
+function doubleInteger(i) {
+    let doubled = i * 2
+    console.log('doubled', doubled)
+    return doubled
+}
+
+doubleInteger(2)
