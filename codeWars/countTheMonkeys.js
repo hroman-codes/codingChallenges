e7ba7bc981d39ac7d812f54aff4844044070cd96
getromandev <getroman.dev@gmail.com>
2021-02-20 23:01:44 -0500

    final solution

---
+++ codeWars/countTheMonkeys.js
@@ -0,0 +1,40 @@
+// [R]epeat
+/**
+ * @tutorial https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+ * @description - you take your some to the forest to see the mon 
+ * @funtion monkeyCount
+ * @param {Integer} n 
+ * @returns {Array} - all numbers up to and including the number n, but exluding zero. 
+ */
+
+// [E]xample 
+
+//     monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+// [A]pproach - iterative approach 
+
+// [C]ode 
+
+ //TODO: creat variable that holds an empty array
+ //TODO: loop up to n
+    //TODO: while looping push n to the stack
+//TODO: return the stack once loops end
+
+function monkeyCount(n) {
+    let stack = [];
+
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+        stack.push(i);
+    }
+
+    console.log(stack)
+    return stack 
+}
+
+// [T]est 
+monkeyCount(10) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// [O]ptimization
+
+⏲️ O(n) Linear Time Complexity 
