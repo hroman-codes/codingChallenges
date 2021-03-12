873bd05b914534de1ef47abbe4d92c5c6f6a12a5
getromandev <getroman.dev@gmail.com>
2021-03-11 23:28:05 -0500

    final solution

---
+++ codeWars/youOnlyNeedOne.js
@@ -0,0 +1,23 @@
+/**
+ * @tutorial https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+ * @function check
+ * @param {Array} a 
+ * @param {Number} x
+ * @returns {Boolean}
+ */
+
+// solution 1 
+function check(a, x) {
+    console.log(a.includes(x));
+}
+
+// solution 2
+function check(a, x) {
+    console.log(a.indexOf(x) > -1 ? true : false);
+}
+
+check([66, 101], 66)
+check(['what', 'a', 'great', 'kata'], 'kat')
+
+// >>> Optimization
+// ⏲️ Time complexity of solution 1 is O(n) Linear time
