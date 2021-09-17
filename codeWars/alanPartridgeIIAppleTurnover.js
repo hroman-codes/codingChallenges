5e14e46f2a1160f41c5127f408bac82b839b375d
getromandev <getroman.dev@gmail.com>
2021-09-16 21:25:08 -0400

    final solution

--- codeWars/alanPartridgeIIAppleTurnover.js
+++ codeWars/alanPartridgeIIAppleTurnover.js
@@ -5,8 +5,35 @@
  * @returns {String}
  */
 
-Input 
+// Input 
+//     >>> 10 
+//     >>> '10'
 
-Process 
+//     - I can use any .Math method for the integer    
+//     - I can apply array like methods to the string 
+//     - I can coerce the string to a number
 
-Complexity
+// Process 
+    // - find the squared of x
+    // - if x is greater than 1000 return 'It's hotter than the sun!!',
+    // - if x is lesss than 1000 return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+function apple(x) {
+
+    let squaredNum = Math.pow(x, 2);
+
+    if (squaredNum > 1000) return "It's hotter than the sun!!"
+    if (squaredNum < 1000) return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+}
+
+
+// Test 
+// console.log(apple(50)) // "It's hotter than the sun!!"
+// console.log(apple('50')) // "It's hotter than the sun!!"
+console.log(apple('3')) // 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+
+// Complexity
+⏲ Time O(1) constant
+🛰 Space O(1) constant
