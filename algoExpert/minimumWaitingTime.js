3011faa0ec9d7a09f534d7dc8133e3283c3e3e81
getromandev <getroman.dev@gmail.com>
2021-09-23 20:32:34 -0400

    add time and space complexity analysis

--- algoExpert/minimumWaitingTime.js
+++ algoExpert/minimumWaitingTime.js
@@ -12,15 +12,15 @@
 // Process 
  function minimumWaitingTime(queries) {
     // sort the queries
-    let sortedQueries = queries.sort((a, b) => a - b)
+    let sortedQueries = queries.sort((a, b) => a - b) // <<< function call to outside  
     // set up a counter variable
     let counter = 0;
     // iterate through the queries
-    for (let i = 0; i < queries.length; i++) {
+    for (let i = 0; i < queries.length; i++) { // <<< Loop
         // grab the value 
         let currentQuery = queries[i]
         // store how many queries are left in the array
-        let remainingQueries = queries.length - (i + 1);
+        let remainingQueries = queries.length - (i + 1); // <<< Operation
         // multiple the curent querie with how many queries are left 
         // append that to the counter variable 
         counter += currentQuery * remainingQueries
@@ -31,4 +31,15 @@
 // Test 
 console.log(minimumWaitingTime([5,1,4]))
 
-// complexity (FOCLP)
+// complexity (FOCL)
+⏲️ Time Complexity = worst case scenerio has a ⏲️ complexity of O(n log (n))
+  
+    line 15: tim sort algo which inherits the insertion and merge sort algos based on sort length/memmory = O(n log (n))
+    line 19: happnes O(n) 
+    line 23: happnes O(n)  
+
+    worst case scenerio has a ⏲️ complexity of O(n log (n))
+
+🛰 Space Complexity = Constant O(1) n being the queries array
+
+    Since we can sort in queries in place without usuing any additional space leaves us with Constant O(1) 
