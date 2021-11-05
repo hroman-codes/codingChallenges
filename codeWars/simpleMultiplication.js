95f58ffa14798e4e6b5b92ee53d57c9cb7e41e29
getromandev <getroman.dev@gmail.com>
2021-11-04 20:52:28 -0400

    final solution

--- codeWars/simpleMultiplication.js
+++ codeWars/simpleMultiplication.js
@@ -8,8 +8,36 @@
 
 // Input 
 
+    // >> 2 
+    //     - is 2 an even number? 
+    //         Yes: 2 * 8 = 16 
+
+    // >> 5 
+    //     - is 5 an even number? 
+    //         No: 5 * 9 = 45
+
 
 // Process 
 
+    // If the number is divisibale by 2 with no remainder then the number is even 
+    //     - divde N by 8 
+    // Otherwise 
+    //     - divide N by 9
+
+function simpleMultiplication(number) {
+
+    if (number % 2 === 0) {
+        return number * 8;
+    } else {
+        return number * 9
+    }
+}
 
 // Output 
+console.log(simpleMultiplication(2)) // 16
+// simpleMultiplication(5) // 45
+
+
+// Complexity FOCLP
+⏲ O(1) Constant time complexity
+🛰 O(1) Constant time complextiy  
