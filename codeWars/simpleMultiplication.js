09b2497fc3427a3a0679a855f7e4b1864f79c3a2
getromandev <getroman.dev@gmail.com>
2021-11-04 20:33:54 -0400

    stub out problem

---
+++ codeWars/simpleMultiplication.js
@@ -0,0 +1,15 @@
+/**
+ * @tutorial https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+ * @description This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+ * @function simpleMultiplication
+ * @param {Number} number 
+ * @returns {String}
+ */
+
+// Input 
+
+
+// Process 
+
+
+// Output 
