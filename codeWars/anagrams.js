10393dadc2e4f85de12f2fddb681213787ea3b19
getromandev <getroman.dev@gmail.com>
2021-11-06 00:48:36 -0400

    setup

---
+++ codeWars/anagrams.js
@@ -0,0 +1,21 @@
+/**
+ * @description Anagrams -  Given two strings, write a function to determine if the second string is an 
+ *                          anagram of the first. An anagram is a word, phrase, or name formed by rearrannging the letters
+ *                          of another, such as cinema, formed from iceman. - frequency counter problem
+ * @function anagram
+ * @param {String} firstWord
+ * @param {String} secondWord
+ * @returns {Boolean}
+ */
+
+// Input 
+
+    >> 'aaz' & 'zza'
+
+// Process
+
+// Output
+console.log(anagram('aaz', 'zza')) // false
+console.log(anagram('anagram', 'nagaram')) // true
+
+// Complexity
