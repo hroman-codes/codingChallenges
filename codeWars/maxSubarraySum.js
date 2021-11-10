924ab78d57e1f1f8f5d56a8ba8684cb707ff179c
getromandev <getroman.dev@gmail.com>
2021-11-10 12:36:59 -0500

    starter code

---
+++ codeWars/maxSubarraySum.js
@@ -0,0 +1,19 @@
+/**
+ * @description write a function called maxSubarraySum which accespts an array of 
+ *              integers and a number called n. The function should calculate the
+ *              maximum sum of n consecutive elements in the array. 
+ * @function maxSubarraySum 
+ * @param {Array} arr 
+ * @param {Number} num
+ * @returns {Number}
+ */
+
+// Input
+
+    >> ([4, 2, 1, 6, ,2], 4)
+
+// Process 
+
+// Output 
+
+// Complexity 
