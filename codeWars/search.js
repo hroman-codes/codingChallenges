2d6688e7ae4976f04064b59ca08e9f85654d3742
getromandev <getroman.dev@gmail.com>
2021-11-11 19:31:35 -0500

    starter code

---
+++ codeWars/search.js
@@ -0,0 +1,17 @@
+/**
+ * @description Given a sorted array of integers, write a function called search,
+ *              that accepts a value and returns the index where ther value passed
+ *              to the function is located. If the valye is not found, return -1
+ * @function search 
+ * @param {Array} arr
+ * @param {Number} num 
+ * @returns {Number}
+ */
+
+// Input
+
+// Process
+
+// Output 
+
+// Complexity
