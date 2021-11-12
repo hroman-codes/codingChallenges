a85cac0118425c60cb54897ece9566442b207c43
getromandev <getroman.dev@gmail.com>
2021-11-12 17:22:56 -0500

    starter code

---
+++ codeWars/findersKeepers.js
@@ -0,0 +1,19 @@
+/**
+ * @description Create a function that looks through an array arr and returns 
+ *              the first element in it that passes a 'truth test'. 
+ *              This means that given an element x, the 'truth test' 
+ *              is passed if func(x) is true. If no element passes the test, 
+ *              return undefined.
+ * @function findElement
+ * @param {Array} arr 
+ * @param {Function} func
+ * @returns {Number}
+ */
+
+// Input 
+
+// Process 
+
+// Output
+
+// Complexity
