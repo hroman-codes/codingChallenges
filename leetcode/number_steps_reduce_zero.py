66c38164cf2eb38591c6462da0c353cc5651bdc3
hroman-codes <romanh99@gmail.com>
2025-06-21 13:50:26 -0400

    final solution

---
+++ leetcode/number_steps_reduce_zero.py
@@ -0,0 +1,21 @@
+""""
+input:
+    14 
+    - can i assume all number are non negative? 
+    - what if I get 0 as an input?
+    - what is the upper bound for num?    
+
+approach:
+    - 
+
+walk through:
+""""
+
+class Solution:
+    def number_of_steps(self, num):
+        pass
+    
+    
+    
+sol = Solution()
+print(sol.number_of_steps(14))
